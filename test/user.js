acb46b6bb9539424c70c0009c9eadf35e1969bd6
lloydbenson <lloyd.benson@gmail.com>
2015-02-15 06:15:55 -0600

    turn this into a plugin

---
+++ test/user.js
@@ -0,0 +1,192 @@
+var Code = require('code');
+var Lab = require('lab');
+var Hapi = require('hapi');
+
+var internals = {
+    defaults: {
+        dirPath: '/tmp/testangler'
+    }
+};
+
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var describe = lab.describe;
+var it = lab.it;
+
+internals.prepareServer = function (callback) {
+
+    var server = new Hapi.Server();
+    server.connection();
+
+    server.register({
+
+        register: require('..'),
+        options: internals.defaults
+    }, function (err) {
+
+        expect(err).to.not.exist();
+        callback(server);
+    });
+};
+
+describe('user', function () {    
+
+    it('POST /api/user lloyd', function (done) {
+
+        internals.prepareServer(function (server) {
+
+            var payload = {
+                name: 'lloyd',
+                displayName: 'Lloyd Benson1',
+                email: 'lloyd.benson@gmail.com'
+            };
+            server.inject({ method: 'POST', url: '/api/user', payload: payload }, function (response) {
+
+                //console.log(response.result);
+                expect(response.statusCode).to.equal(200);
+                expect(response.result.id).to.exist();
+                done();
+            });
+        });
+    });
+
+    it('POST /api/user backer', function (done) {
+
+        internals.prepareServer(function (server) {
+
+            var payload = {
+                name: 'backer',
+                displayName: 'Ben Acker',
+                email: 'ben.acker@gmail.com'
+            };
+            server.inject({ method: 'POST', url: '/api/user', payload: payload }, function (response) {
+
+                //console.log(response.result);
+                expect(response.statusCode).to.equal(200);
+                expect(response.result.id).to.exist();
+                done();
+            });
+        });
+    });
+
+    it('GET /api/users', function (done) {
+
+        internals.prepareServer(function (server) {
+            server.inject({ method: 'GET', url: '/api/users'}, function (response) {
+
+                //console.log(response);
+                expect(response.statusCode).to.equal(200);
+                expect(response.result).to.have.length(2);
+                done();
+            });
+        });
+    });
+
+    it('GET /api/user/{id}', function (done) {
+
+        internals.prepareServer(function (server) {
+
+            server.inject({ method: 'GET', url: '/api/users'}, function (response) {
+
+                expect(response.statusCode).to.equal(200);
+                var id = response.result[0].id;
+                server.inject({ method: 'GET', url: '/api/user/' + id}, function (response) {
+
+                    //console.log(response.result);
+                    expect(response.result.id).to.exist();
+                    expect(response.result.name).to.equal('lloyd');
+                    expect(response.result.displayName).to.equal('Lloyd Benson1');
+                    expect(response.statusCode).to.equal(200);
+                    done();
+                });
+            });
+        });
+    });
+
+    it('GET /api/user/byname/{name}', function (done) {
+
+        internals.prepareServer(function (server) {
+
+            server.inject({ method: 'GET', url: '/api/users'}, function (response) {
+
+                expect(response.statusCode).to.equal(200);
+                var id = response.result[0].id;
+                var name = response.result[0].name;
+                server.inject({ method: 'GET', url: '/api/user/byname/' + name}, function (response) {
+
+                    //console.log(response.result);
+                    expect(response.result.id).to.exist();
+                    expect(response.result.id).to.equal(id);
+                    expect(response.statusCode).to.equal(200);
+                    done();
+                });
+            });
+        });
+    });
+
+    it('POST /api/user/{id}', function (done) {
+
+        internals.prepareServer(function (server) {
+
+            server.inject({ method: 'GET', url: '/api/users'}, function (response) {
+
+                expect(response.statusCode).to.equal(200);
+                var id = response.result[0].id;
+                var payload = {
+                    displayName: 'Lloyd Benson'
+                };
+                server.inject({ method: 'POST', url: '/api/user/' + id, payload: payload}, function (response) {
+
+                    //console.log(response.result);
+                    expect(response.statusCode).to.equal(200);
+                    expect(response.result.displayName).to.equal('Lloyd Benson');
+                    done();
+                });
+            });
+        });
+    });
+
+    it('DELETE /api/user/{id} lloyd', function (done) {
+
+        internals.prepareServer(function (server) {
+
+            server.inject({ method: 'GET', url: '/api/users'}, function (response) {
+
+                expect(response.statusCode).to.equal(200);
+                expect(response.result).to.have.length(2);
+                var id = response.result[0].id;
+                server.inject({ method: 'DELETE', url: '/api/user/' + id}, function (response) {
+
+                    expect(response.statusCode).to.equal(200);
+                    server.inject({ method: 'GET', url: '/api/users'}, function (response) {
+
+                        expect(response.result).to.have.length(1);
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    it('DELETE /api/user/{id} backer', function (done) {
+
+        internals.prepareServer(function (server) {
+
+            server.inject({ method: 'GET', url: '/api/users'}, function (response) {
+
+                expect(response.statusCode).to.equal(200);
+                expect(response.result).to.have.length(1);
+                var id = response.result[0].id;
+                server.inject({ method: 'DELETE', url: '/api/user/' + id}, function (response) {
+
+                    expect(response.statusCode).to.equal(200);
+                    server.inject({ method: 'GET', url: '/api/users'}, function (response) {
+
+                        expect(response.result).to.have.length(0);
+                        done();
+                    });
+                });
+            });
+        });
+    });
+});
