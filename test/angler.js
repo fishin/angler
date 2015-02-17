5d78f5d44d4cfad914c3b224a1a2cd0f5e7c7c23
lloydbenson <lloyd.benson@gmail.com>
2015-02-17 06:18:24 -0600

    enhance angler to have validate function and store enc passwords

--- test/angler.js
+++ test/angler.js
@@ -23,13 +23,23 @@ describe('angler', function () {
         var config = {
             name: 'lloyd',
             displayName: 'Lloyd Benson1',
-            email: 'lloyd.benson@gmail.com'
+            email: 'lloyd.benson@gmail.com',
+            password: 'password1'
         };
         var createUser = angler.createUser(config);
         expect(createUser.id).to.exist();
         expect(createUser.name).to.equal('lloyd');
         expect(createUser.displayName).to.equal('Lloyd Benson1');
         expect(createUser.email).to.equal('lloyd.benson@gmail.com');
+        expect(createUser.password.length).to.equal(60);
+        done();
+    });
+
+    it('validatePassword lloyd', function (done) {
+
+        var hash = angler.getUserByName('lloyd').password;
+        var validate = angler.validatePassword('password', hash);
+        expect(validate).to.be.false();
         done();
     });
 
@@ -38,7 +48,8 @@ describe('angler', function () {
         var config = {
             name: 'backer',
             displayName: 'Ben Acker',
-            email: 'ben.acker@gmail.com'
+            email: 'ben.acker@gmail.com',
+            password: 'password1'
         };
         var createUser = angler.createUser(config);
         expect(createUser.id).to.exist();
@@ -76,10 +87,18 @@ describe('angler', function () {
             displayName: 'Lloyd Benson'
         }
         var updateUser = angler.updateUser(getUser.id, payload);
-        expect(updateUser.id).to.exist();
-        expect(updateUser.name).to.equal('lloyd');
         expect(updateUser.displayName).to.equal('Lloyd Benson');
-        expect(updateUser.email).to.equal('lloyd.benson@gmail.com');
+        done();
+    });
+
+    it('updateUser lloyd', function (done) {
+
+        var getUser = angler.getUserByName('lloyd');
+        var payload = {
+            password: 'password'
+        }
+        var updateUser = angler.updateUser(getUser.id, payload);
+        expect(updateUser.password).to.not.equal(getUser.password);
         done();
     });
 
@@ -87,6 +106,14 @@ describe('angler', function () {
 
         var getUsers = angler.getUsers();
         expect(getUsers).to.be.length(2);
+        done();
+    });
+
+    it('validatePassword lloyd', function (done) {
+
+        var hash = angler.getUserByName('lloyd').password;
+        var validate = angler.validatePassword('password', hash);
+        expect(validate).to.be.true();
         done();
     });
 
@@ -103,5 +130,4 @@ describe('angler', function () {
         angler.deleteUser(getUser.id);
         done();
     });
-
 });
